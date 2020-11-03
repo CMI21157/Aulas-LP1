38377e092d525891db33be80498df63e87494218
CMI21157 <73717875+CMI21157@users.noreply.github.com>
2020-11-03 19:01:48 +0000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+/**
+ * @file main.c
+ * @author Carlos Barbosa (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-11-03
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int arcg, char *argc[]) {
+
+    //variáveis
+    int num1;
+    int num2;
+    int num3;
+    int num4;
+    int num5;
+
+    //atribuição de valores às variáveis
+    num1=20;
+    num2=8;
+    num3=5;
+    num4=2;
+    
+    //escrever na consola o resultado
+    printf("num1+num2*num3/num4=%d, num5");
+
+    //pausa
+    getchar();    
+}
